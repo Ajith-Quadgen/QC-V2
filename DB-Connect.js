0dc4992e5d2e0c4a23cc7475963e234ded90d761
ajith venkatesh <ajith.venkatesh@quadgenwireless.com>
2023-11-14 10:28:32 +0530

    Notification  Module Push

--- DB-Connect.js
+++ DB-Connect.js
@@ -3,11 +3,11 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '@Renu001',
-    database: 'qc-portal',
+    // password: '@Renu001',
+    // database: 'qc-portal',
 
-    // password:'@Quadgen23',
-    // database: 'qc-testing-db',
+    password:'@Quadgen23',
+    database: 'qc-qc-portal',
     
     multipleStatements: true
   });
